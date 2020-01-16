0b0dddda2189594ef6a3d8782fa41e2fe594479f
Barcellos-Felipe <barcellossfelipe@gmail.com>
2020-01-15 23:20:02 -0400

    Adicionando Relacao_Area_Por_ProfissaoModel.cs

---
+++ WebApiBackendTeste/Model/Relacao_Area_Por_ProfissaoModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiBackendTeste.Model {
+    public class Relacao_Area_Por_ProfissaoModel {
+        [Table("Relacao_Area_Por_Profissao")]
+        public class Relacao_Area_Por_Profissao {
+            /// <summary>
+            /// 
+            /// </summary>
+            [Key]
+            [Column("IdRelacao_Area_Por_Profissao")]
+            public int IdRelacao_Area_Por_Profissao { get; set; }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            [Column("IdAreaConhecimento")]
+            [Required(ErrorMessage = "A Área de Conhecimento é Obrigatória", AllowEmptyStrings = false)]
+            public string IdAreaConhecimento { get; set; }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            [Column("IdProfissao")]
+            [Required(ErrorMessage = "O nome da Profisão é Obrigatório", AllowEmptyStrings = false)]
+            public int IdProfissao { get; set; }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            [ForeignKey("IdAreaConhecimento")]
+            public AreaConhecimento AreaConhecimento { get; private set; }
+
+            [ForeignKey("IdProfissao")]
+            public Profissao Profissao { get; private set; }
+
+        }
+    }
+}
